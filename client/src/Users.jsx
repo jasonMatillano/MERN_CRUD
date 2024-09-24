c9cdcd4095682111537460d6160e72ba5610fa94
Jason <jason.matillano@gmail.com>
2024-09-25 02:58:18 +0800

    Final

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -3,36 +3,35 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Users() {
-
-    const  [users, setUsers] = useState([
-        {}
-    ]);
-    
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:3001')
-        .then (result => {
-            setUsers(result.data)
-        }).catch(err => {
-            console.log(err)
-        })
-    }, [])
+            .then(result => {
+                setUsers(result.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
 
     const handleDelete = (id) => {
         axios.delete('http://localhost:3001/delete/' + id)
-        .then (result => {
-            console.log(result)
-            window.location.reload()
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-    
+            .then(result => {
+                console.log(result);
+                window.location.reload();
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
     return (
         <div className='d-flex vh-100 bg-light justify-content-center align-items-center'>
             <div className='w-75 bg-white rounded p-4 shadow'>
-                {/* <h4 className='mb-0'>User List</h4> */}
-                <Link to={`/create`} className='btn btn-success  ms-auto'>Add User</Link>
+                <div className="d-flex justify-content-start mb-3">
+                    <Link to={`/create`} className='btn btn-success'>Add User</Link>
+                </div>
                 <table className='table table-bordered table-hover'>
                     <thead className='table-dark'>
                         <tr>
